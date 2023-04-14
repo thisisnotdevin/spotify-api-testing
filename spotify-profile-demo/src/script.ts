83eec66a5a5f4c87f6c7aa87c03e49f10ae484f7
thisisnotdevin <devin1299@outlook.com>
2023-04-13 23:43:36 -0400

    foll

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -83,12 +83,15 @@ function populateUI(profile: any) {
         profileImage.src = profile.images[0].url;
         document.getElementById("avatar")!.appendChild(profileImage);
     }
-    document.getElementById("id")!.innerText = profile.id;
+
     document.getElementById("email")!.innerText = profile.email;
     document.getElementById("uri")!.innerText = profile.uri;
     document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
     document.getElementById("url")!.innerText = profile.href;
     document.getElementById("url")!.setAttribute("href", profile.href);
     document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
-    document.getElementById("followers")!.setAttribute("total", profile.followers.total);
+    document.getElementById("followers")!.innerText = profile.followers.total;
+    document.getElementById("country")!.innerText = profile.country;
+    document.getElementById("product")!.innerText = profile.product;
+    document.getElementById("type")!.innerText = profile.type;
 }
